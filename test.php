ac7cb2edd5aab6259815bc1db2cf821d322c5173
Vitaliy Moskalenko <to-@mail.ru>
2016-10-28 14:15:16 +0400

    carcass

---
+++ test.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html><head>
+  
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+	<meta charset="utf-8">
+	
+</head>
+<body>
+
+  <h1>Test</h1>
+
+  <?php  echo '<br><h2>PHP Test</h2>'; ?>
+
+</body></html>	
